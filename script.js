b48c529a0163da198bb71c8fa9a0e9f63f3cb8e3
GeethPraneethsai <gpsgeeth02@gmail.com>
2025-12-31 17:22:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_KEY = ''; 
+const API_KEY = 'AIzaSyASOZOjztwenjYErH8G_snR1iNTVDVbJFA'; 
 const API_URL = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent';
 
 
